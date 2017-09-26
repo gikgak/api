37cd68cabe16b047e36d51858a2a05d896c823d2
Natacha sanquer <natachasanquer@free.fr>
2017-09-26 19:21:38 +0200

    Create push entity

---
+++ api/models/Wish.js
@@ -0,0 +1,18 @@
+/**
+ * Wish.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+//Permet d'obliger les attributs de la methode d'etre = attributes
+	schema : true,
+  attributes: {
+	title : 'string',
+	isChecked : 'boolean',
+
+  },
+
+};
+
