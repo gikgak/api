24f9a5d6bba43a579da35f797f69a71031917ccf
Yann Bertrand <yann@bertrand.com>
2017-09-27 12:28:14 -0400

    Create the Lover entity

---
+++ api/models/Lover.js
@@ -0,0 +1,27 @@
+/**
+ * Lover.js
+ *
+ * @description :: Must be associated with an already existing group
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  schema : true,
+
+  attributes: {
+
+    nickname: {
+      type: 'string',
+      required: true,
+    },
+
+    group: {
+      model: 'group',
+      required: true,
+    },
+
+  },
+
+};
+
